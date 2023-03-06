9d2778f3cb1ccaebc1d47b0e835bdd50d163913b
Jose Retana <joseantonioretanacorrales@gmail.com>
2023-03-05 20:48:28 -0600

    Fixed logic and finished test bench for exp3

--- lab2/exp3/countdown_nbits.sv
+++ lab2/exp3/countdown_nbits.sv
@@ -1,22 +1,23 @@
-module countdown_nbits (
+module countdown_nbits 
 
+#( parameter N = 6 ) 
+
+(
 	input logic clk,        
 	input logic reset,        
 	output reg[N-1:0] countdownOut,
-	output wire [6:0] led2,
-	output wire [6:0] led1
+	output logic [6:0] led2,
+	output logic [6:0] led1
 	
 );
- 
+    
 
-   parameter N = 6;    
-
-	reg[N-1:0] countdown;   
+	reg[N-1:0] countdown = (2**N)-1;   
 
 
-	localparam [9:0] segment_const = {
-	   7'b1111110, // 0
-	   7'b0110000, // 1
+	localparam [15:0] segment_const = {
+		7'b1111110, // 0
+		7'b0110000, // 1
 	   7'b1101101, // 2
 	   7'b1111001, // 3
 	   7'b0110011, // 4
@@ -28,35 +29,37 @@ module countdown_nbits (
 	  	7'b1110111, // A
 	  	7'b0011111, // b
 	  	7'b1001110, // C
-	  	7'b0111101, // D
+	  	7'b0111101, // d
 	  	7'b1001111, // E
 	  	7'b1000111  // F
 	};
 
-
-
-	always @(posedge clk) 
+	
+	always @(posedge clk or posedge reset) 
 		begin
 			if (reset) 
 				begin     
-					countdown <= (2**N)-1;
+					countdown = (2**N)-1;
+					countdownOut = countdown;
+					led2 = ~segment_const[countdownOut[5:4]];
+					led1 = ~segment_const[countdownOut[3:0]];
 				end
 				
 			else
 				begin
 					if (countdown == 0) 
 						begin   
-							countdown <= (2**N)-1;
-							countdownOut <= countdown;
-							led2 = segment_const[countdownOut[5:4]];
-							led1 = segment_const[countdownOut[3:0]];
+							countdown = (2**N)-1;
+							countdownOut = countdown;
+							led2 = ~segment_const[countdownOut[5:4]];
+							led1 = ~segment_const[countdownOut[3:0]];
 						end
 					else 
 						begin       
-							countdown <= countdown - 1;
-							countdownOut <= countdown;
-							led2 = segment_const[countdownOut[5:4]];
-							led1 = segment_const[countdownOut[3:0]];
+							countdown = countdown - 1;
+							countdownOut = countdown;
+							led2 = ~segment_const[countdownOut[5:4]];
+							led1 = ~segment_const[countdownOut[3:0]];
 						end
 				end
 		end
--- lab2/exp3/countdown_nbits_tb.sv
+++ lab2/exp3/countdown_nbits_tb.sv
@@ -1,31 +1,67 @@
-//module countdown_nbits_tb;
-//
-//
-//parameter CLK = 10; 
-//
-//reg clk;
-//reg rst;
-//wire [N-1:0] countdownOut;
-//
-//
-//countdown_timer dut (
-//  .clk(clk),
-//  .rst(rst),
-//  .countdownOut(countdownOut)
-//);
-//
-//
-//initial begin
-//
-//// test 2 bits
-//
-//
-//
-//
-//
-//
-//
-//
-//end
-//
-//endmodule
+
+module countdown_2bits_tb;
+
+
+parameter CLK = 10; 
+
+	reg clk;
+	reg reset;
+	wire [1:0] countdownOut2bits;
+	wire [3:0] countdownOut4bits;
+	wire [5:0] countdownOut6bits;
+
+	countdown_nbits #(.N(2)) countdown2bits
+	
+	(
+	  .clk(clk),
+	  .reset(reset),
+	  .countdownOut(countdownOut2bits)
+	);
+	
+	countdown_nbits #(.N(4)) countdown4bits
+	
+	(
+	  .clk(clk),
+	  .reset(reset),
+	  .countdownOut(countdownOut4bits)
+	);
+
+	countdown_nbits #(.N(6)) countdown6bits
+	
+	(
+	  .clk(clk),
+	  .reset(reset),
+	  .countdownOut(countdownOut6bits)
+	);
+
+	initial begin
+	
+		$display("Testing countdown timers...");
+		clk = 1'b0;
+		reset = 1'b0;
+		
+		repeat (120) begin
+			  #5 clk = !clk;
+		end
+
+		#10;
+		
+		$display("activating reset...");
+		reset = 1'b1;
+		
+		assert (countdownOut2bits === 3) else $error ("2-bit countdown reset failed!!!!");
+		assert (countdownOut4bits === 15) else $error ("4-bit countdown reset failed!!!!");
+		assert (countdownOut6bits === 63) else $error ("6-bit countdown reset failed!!!!");
+		
+		repeat (20) begin
+			  #5 clk = !clk;
+		end
+		
+		#5;
+		
+		$display("Countdown timer test finished!");
+	
+
+	end
+
+endmodule
