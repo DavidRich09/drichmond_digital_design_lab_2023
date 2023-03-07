1e901e5b66c0522add7e7e11c164f75cc6630bfd
Sormocs <sermartbo@gmail.com>
2023-03-07 00:01:03 -0600

    Bugfix

--- lab2/exp3/countdown_nbits.sv
+++ lab2/exp3/countdown_nbits.sv
@@ -1,64 +1,49 @@
-module countdown_nbits (
+module countdown_nbits 
 
-	input clk,        
-	input reset,        
+#( parameter N = 6 ) 
+
+(
+	input logic clk,        
+	input logic reset,        
 	output reg[N-1:0] countdownOut,
-	output wire [6:0] led2,
-	output wire [6:0] led1
+	output logic [6:0] led2,
+	output logic [6:0] led1
 	
 );
- 
+    
+	 
 
-   parameter N = 6;    
+	reg[N-1:0] countdown = (2**N)-1;   
 
-	reg[N-1:0] countdown;   
-
-
-	localparam [9:0] segment_const = {
-	   7'b1111110, // 0
-	   7'b0110000, // 1
-	   7'b1101101, // 2
-	   7'b1111001, // 3
-	   7'b0110011, // 4
-	   7'b1011011, // 5
-	   7'b1011111, // 6
-	   7'b1110000, // 7
-	   7'b1111111, // 8
-	   7'b1111011, // 9
-	  	7'b1110111, // A
-	  	7'b0011111, // b
-	  	7'b1001110, // C
-	  	7'b0111101, // D
-	  	7'b1001111, // E
-	  	7'b1000111  // F
-	};
-
-
-
-	always @(posedge clk) 
+	display_converter converter4led1(countdownOut[3], countdownOut[2], countdownOut[1], countdownOut[0], led1);
+	display_converter converter4led2(1'b0, 1'b0, countdownOut[5], countdownOut[4], led2);
+		
+	always @(posedge clk or posedge reset) 
 		begin
 			if (reset) 
 				begin     
-					countdown <= (2**N)-1;
+					countdown = (2**N)-1;
+					countdownOut = countdown;
 				end
 				
 			else
 				begin
 					if (countdown == 0) 
 						begin   
-							countdown <= (2**N)-1;
-							countdownOut <= countdown;
-							led2 = segment_const[countdownOut[5:4]];
-							led1 = segment_const[countdownOut[3:0]];
+							countdown = (2**N)-1;
+							countdownOut = countdown;
 						end
 					else 
 						begin       
-							countdown <= countdown - 1;
-							countdownOut <= countdown;
-							led2 = segment_const[countdownOut[5:4]];
-							led1 = segment_const[countdownOut[3:0]];
+							countdown = countdown - 1;
+							countdownOut = countdown;
 						end
 				end
 		end
+	
+	
+
+			
+
 	
 endmodule
