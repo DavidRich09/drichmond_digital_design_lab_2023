97378ad398dbb3daa324635b6e50e8df2012630c
Jose Retana <joseantonioretanacorrales@gmail.com>
2023-03-27 16:42:15 -0600

    CLK aparece en el timing analyzer

--- lab3/Problema 2/register_output.sv
+++ lab3/Problema 2/register_output.sv
@@ -1,10 +1,10 @@
 module register_output  #(parameter N=4)(
 													 input [N-1:0] res,
-													 input clk, reset, reg_flag,zr_flag, cry_flag, of_flag,
+													 input clk, reset, neg_flag,zr_flag, cry_flag, of_flag,
 													 output reg reg_neg_flag, reg_zr_flag, reg_cry_flag, reg_of_flag,
 													 output reg [N-1:0] regOutput);
 													 
-	always @(posedge clk or posedge reset) begin
+	always @(negedge clk or posedge reset) begin
 	
 		if(reset) begin
 			reg_neg_flag <= 0;
@@ -14,7 +14,7 @@ module register_output  #(parameter N=4)(
 			regOutput <= 0;
 			end
 		else begin
-			reg_neg_flag <= reg_flag;
+			reg_neg_flag <= neg_flag;
 			reg_zr_flag <= zr_flag;
 			reg_cry_flag <= cry_flag;
 			reg_of_flag <= of_flag;
