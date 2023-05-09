194a25ee5c2371310eb45579cf9fb14515816cd7
David Richmond Soto <luis.richmond9@gmail.com>
2023-05-08 23:16:53 -0600

    reset hotfix
    reset hotfix

--- lab4/reset.sv
+++ lab4/reset.sv
@@ -7,18 +7,17 @@ module reset (
     logic [15:0][3:0] temp_input;
     logic [15:0][3:0] temp_matrix1, temp_matrix2;
     integer i;
-     
-    add_random random_inst_1 (.cell_matrix_in(temp_input), .cell_matrix_out(temp_matrix1));
-    add_random random_inst_2 (.cell_matrix_in(temp_matrix1), .cell_matrix_out(temp_matrix2));
-
-    always @(posedge reset_input) begin
-
+	 
+	 always @(posedge reset_input) begin
         for (i=0; i<16; i=i+1) begin
-            temp_input[i] = 4'd0;
+            temp_input[i] = 4'b0;
         end
-
-        random_inst_1.cell_matrix_in <= temp_input;   // asignar temp_input a la entrada de random_inst_1
-        random_inst_2.cell_matrix_in <= temp_matrix1;  // asignar la salida de random_inst_1 a la entrada de random_inst_2
-        cell_matrix_out <= temp_matrix2;              // asignar la salida de random_inst_2 a la salida del modulo reset
     end
+	 
+	 add_random random_inst_1 (.cell_matrix_in(temp_input), .cell_matrix_out(temp_matrix1));
+    add_random random_inst_2 (.cell_matrix_in(temp_matrix1), .cell_matrix_out(temp_matrix2));
+	 
+	 assign cell_matrix_out = temp_matrix2;
+	 
+
 endmodule
