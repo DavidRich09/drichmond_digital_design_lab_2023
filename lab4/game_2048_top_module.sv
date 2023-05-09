01436b2c69bbd541d37b1f45b54ef8c9ded56ef8
Sormocs <sermartbo@gmail.com>
2023-05-09 04:50:02 -0600

    VGA + Logic

--- lab4/game_2048_top_module.sv
+++ lab4/game_2048_top_module.sv
@@ -1,13 +1,20 @@
 module game_2048_top_module 	
 
 (
+	input logic win_a,
+	input logic win_b,
+	input logic win_c,
+	input logic win_d,
 	input logic reset_switch_signal,
 	input logic up_button_signal,        
 	input logic down_button_signal,
 	input logic left_button_signal,
 	input logic right_button_signal,
-	input logic ME_clk  
-	// output cosas de vga ???? no sé @Sergio ahí usted hace la vara
+	input logic ME_clk,clk,  
+	
+	output logic vga_hsync,vga_vsync,sync_blank,sync_b,
+	output logic [7:0]red,green,blue,
+	output logic clk_25
 );
 	
 	logic reset_State_Wire;
@@ -21,13 +28,14 @@ module game_2048_top_module
 	logic right_State_Wire;
 	
 	logic [15:0][3:0] cell_matrix_bus;
+	logic [12:0] points_out;
 	
-	maquina_Estados ME(	clk,
+	maquina_Estados ME(	ME_clk,
 								reset_switch_signal,
-								up_button_signal,        
-								down_button_signal,
-								left_button_signal,
-								right_button_signal,
+								~up_button_signal,        
+								~down_button_signal,
+								~left_button_signal,
+								~right_button_signal,
 								logic_lose_Wire,
 								logic_win_Wire,
 								
@@ -41,24 +49,37 @@ module game_2048_top_module
 							);
 
 	logic_Module 	LM(
+								win_a,
+								win_b,
+								win_c,
+								win_d,
 								up_State_Wire,        
 								down_State_Wire,
 								left_State_Wire,
 								right_State_Wire,
+								reset_State_Wire,
 								logic_lose_Wire,
 								logic_win_Wire,
-								
-								cell_matrix_bus
-								
-								//input logic clk (????)   
+								points_out,
+								cell_matrix_bus   
 								
 							);
-	
+							
 	VGA_Main_Module VGA(
 
-								cell_matrix_bus
-								
-								//output logic ????? @Sergio ahí ve que hace
+								clk,
+								reset_State_Wire,
+								win_state_Wire,
+								lose_state_Wire,
+								cell_matrix_bus,
+								vga_hsync,
+								vga_vsync,
+								sync_blank,
+								sync_b,
+								red,
+								green,
+								blue,
+								clk_25
 								 
 							);
 
