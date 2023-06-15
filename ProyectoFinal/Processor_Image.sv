8732d4191642af665604e89012a46d189140b913
Sormocs <sermartbo@gmail.com>
2023-06-15 04:00:55 -0600

    VGA Con

--- ProyectoFinal/Processor_Image.sv
+++ ProyectoFinal/Processor_Image.sv
@@ -1,9 +1,18 @@
 module Processor_Image 	
 
 (
-   
-
+	input logic clk,rst,
+	output logic vga_hsync,vga_vsync,sync_blank,sync_b,
+	output logic [7:0]red,green,blue,
+	output logic clk_25
 );
+
+	logic [9:0]hs;
+	logic [9:0]vs;
+	
+	clkdiv div(clk,clk_25);
+			
+	ControladorGrafico cgr(clk_25,vga_hsync,vga_vsync,sync_blank,sync_b,hs,vs);
 
 
 
